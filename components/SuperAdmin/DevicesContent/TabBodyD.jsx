29a9f3196832f48441a61047c4d4da8172c49709
esmat1103 <esmateddinekhamassi@gmail.com>
2024-06-29 13:58:40 -1100

    esmat

---
+++ components/SuperAdmin/DevicesContent/TabBodyD.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import export1 from '../../../public/assets/Table/export.svg';
+import edit from '../../../public/assets/Table/edit.svg';
+import clear from '../../../public/assets/Table/delete.svg';
+import DeleteConfirmation from '@components/Commun/popups/Devices/DeleteConfirmationModal';
+import SensorsPopup from '@components/Commun/Popups/Devices/SensorsPopup';
+
+const TableBodyD = ({ tableData, handleEdit, selectedRows, handleCheckboxChange }) => {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [deleteItem, setDeleteItem] = useState(null);
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const handleDelete = (id) => {
+    const itemToDelete = tableData.find(item => item.id === id);
+    setDeleteItem(itemToDelete);
+    setShowDeleteConfirmation(true);
+  };
+
+  const confirmDelete = () => {
+   
+    setShowDeleteConfirmation(false);
+  };
+
+  const cancelDelete = () => {
+    setShowDeleteConfirmation(false);
+  };
+  const handleButtonClick = () => {
+    setIsPopupVisible(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupVisible(false);
+  };
+  
+  return (
+    <>
+
+    <tbody className='darkgrey'>
+      {tableData.map((row) => (
+        <tr 
+          key={row.id} 
+          className="table-row-box  nunito" 
+        >
+          <td>
+            <input 
+              type="checkbox" 
+              className="custom-checkbox" 
+              checked={selectedRows.includes(row.id)} 
+              onChange={() => handleCheckboxChange(row.id)} 
+            />
+          </td>
+          <td className="centered-cell"><div className="box-cell f11">{row.id}</div></td>
+          <td className='f11 nunito pl23 darkgrey '>{row.deviceName}</td>
+          <td className='f11 nunito  fw600 darkgrey'>{row.macAddress}</td>
+          <td className='f11 nunito pl17'><div className="box-cell f11">{row.location}</div></td>
+          <td className='f11 nunito pl23'>{row.admin}</td>
+          <td className='f11 nunito pl50'>
+            {row.clients.split(',').map((client, index) => (
+              <div key={index}>
+                {client}
+              </div>
+            ))}
+          </td>
+          <td className='f11 nunito pl23'>
+              <button className="box-cell f11 button-like" onClick={handleButtonClick}>
+                {row.sensors}
+              </button>
+            </td>
+          <td className="f11 nunito  ">
+            <div className={row.state === 'Enabled' ? 'box-cellE f11' : 'box-cellD f11'}>
+              {row.state}
+            </div>
+          </td>
+          <td className="text-center">
+            <div className="flex justify-center ">
+            <button onClick={() => handleEdit(row.id)}>
+                <Image src={edit} alt='edit' width={20} height={20} />
+              </button>
+              <button onClick={() => handleEdit(row.id)}>
+                <Image src={export1} alt='export' width={20} height={20} />
+              </button>
+              <button onClick={() => handleDelete(row.id)}>
+                <Image src={clear} alt='delete' width={20} height={20} />
+              </button>
+            </div>
+          </td>
+          </tr>
+        ))}
+      </tbody>
+      {showDeleteConfirmation && (
+        <DeleteConfirmation 
+          item={deleteItem} 
+          onConfirmDelete={confirmDelete} 
+          onCancelDelete={cancelDelete} 
+        />
+      )}
+      {isPopupVisible && <SensorsPopup closePopup={closePopup} />}
+    </>
+  );
+};
+
+export default TableBodyD;
